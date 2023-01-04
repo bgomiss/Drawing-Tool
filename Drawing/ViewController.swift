163b0fd3b74074f8e15d4b86af5834453246322e
bgomiss <aduskun@gmail.com>
2023-01-05 00:01:14 +0300

    fixed the issue the pencil not changing color

--- Drawing/ViewController.swift
+++ Drawing/ViewController.swift
@@ -7,43 +7,99 @@
 
 import UIKit
 import PencilKit
+import PhotosUI
 
-class ViewController: UIViewController, PKCanvasViewDelegate {
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
 
-    private let canvasView: PKCanvasView = {
-       
-        let canvas = PKCanvasView()
-        canvas.drawingPolicy = .anyInput
-        return canvas
-    }()
     
+    
+    @IBOutlet weak var pencilButton: UIBarButtonItem!
+    
+    @IBOutlet weak var canvasView: PKCanvasView!
+    
+    let canvasWidth: CGFloat = 350
+    let canvasOverScrollHeight: CGFloat = 500
     let drawing = PKDrawing()
+    let toolPicker = PKToolPicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         canvasView.drawing = drawing
         canvasView.delegate = self
-        view.addSubview(canvasView)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        canvasView.frame = view.bounds
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+        canvasView.alwaysBounceVertical = true
+        canvasView.drawingPolicy = .anyInput
         
-        let toolPicker = PKToolPicker()
+        }
+    
+    
+   override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+       
         toolPicker.setVisible(true, forFirstResponder: canvasView)
         toolPicker.addObserver(canvasView)
         canvasView.becomeFirstResponder()
+    }
+   
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let canvasScale = canvasView.bounds.width / canvasWidth
+        canvasView.minimumZoomScale = canvasScale
+        canvasView.maximumZoomScale = canvasScale
+        canvasView.zoomScale = canvasScale
+        
+        updateContentSizeForDrawing()
+        canvasView.contentOffset = CGPoint(x: 0, y: -canvasView.adjustedContentInset.top)
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool{
+        return true
+    }
+    
+    @IBAction func fingerOrPencil (_ sender: Any) {
+        canvasView.allowsFingerDrawing.toggle()
+        pencilButton.title = canvasView.allowsFingerDrawing ? "Finger" : "Pencil"
+        
+    }
+    
+    
+    @IBAction func saveToCameraRoll(_ sender: Any) {
+        
+        UIGraphicsBeginImageContextWithOptions(canvasView.bounds.size, false, UIScreen.main.scale)
+        canvasView.drawHierarchy(in: canvasView.bounds, afterScreenUpdates: true)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        if image != nil {
+            PHPhotoLibrary.shared().performChanges({
+                PHAssetChangeRequest.creationRequestForAsset(from: image!)
+            }, completionHandler: {success, error in
+            
+            })
+        }
+    }
+    
+    
+    
+    func updateContentSizeForDrawing() {
+        let drawing = canvasView.drawing
+        let contentHeight: CGFloat
+        
+        if !drawing.bounds.isNull {
+            contentHeight = max(canvasView.bounds.height, (drawing.bounds.maxY + self.canvasOverScrollHeight) * canvasView.zoomScale)
+        } else {
+            contentHeight = canvasView.bounds.height
+        }
+        canvasView.contentSize = CGSize(width: canvasWidth * canvasView.zoomScale, height: contentHeight)
     }
     
     // Delegate Methods
     
     func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
-        
+       updateContentSizeForDrawing()
+      
     }
     func canvasViewDidEndUsingTool(_ canvasView: PKCanvasView) {
         
